216b15970225f9058f36ce6690552479f48c9c77
nikoleistar <51000747+nikoleistar@users.noreply.github.com>
2019-10-18 12:10:38 +0300

    Initial commit

---
+++ lesson__1__q2.js
@@ -0,0 +1,8 @@
+//  2. Работа с переменными.
+//  3. Объявить две переменные: admin и name. Записать в name строку "Василий";
+//  Скопировать значение из name в admin. Вывести admin (должно вывести «Василий») 
+"use strict";
+let admin, names;
+names = "Василий";
+admin = name;
+console.log(admin);
