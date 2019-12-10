4277856186edea29d29c1852fe2c0328bcfc994d
nikoleistar <51000747+nikoleistar@users.noreply.github.com>
2019-12-10 19:00:37 +0300

    домашка

--- lesson__1__q2.js
+++
@@ -1,8 +0,0 @@
-//  2. Работа с переменными.
-//  3. Объявить две переменные: admin и name. Записать в name строку "Василий";
-//  Скопировать значение из name в admin. Вывести admin (должно вывести «Василий») 
-"use strict";
-let admin, names;
-names = "Василий";
-admin = name;
-console.log(admin);
